53d90bcc64d8bd7f90f6d4a131106c8956c44c78
jaime48 <duyanguk@163.com>
2018-01-16 20:54:02 +0800

    modification

--- Routes.py
+++ Routes.py
@@ -2,21 +2,6 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }]
-
-
 @app.route('/')
 def index():
     return 'Hello World'
